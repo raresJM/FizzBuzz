0a830fc4ffe4350987605cbcfb4b66a8366548fb
raresJM <rares.scarlat.jm@gmail.com>
2017-02-17 01:41:03 +0200

    created test methods

--- FizzBuzz.cs
+++ FizzBuzz.cs
@@ -7,8 +7,58 @@ namespace FizzBuzz2
     public class FizzBuzz
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCheckDivBy3_GoodScenario_01()
         {
+            Assert.IsTrue(checkIfDivBy3(3));
         }
+        [TestMethod]
+        public void TestCheckDivBy3_GoodScenario_02()
+        {
+            Assert.IsTrue(checkIfDivBy3(15));
+        }
+
+        [TestMethod]
+        public void TestCheckDivBy3_BadScenario_01()
+        {
+            Assert.IsFalse(checkIfDivBy3(2));
+        }
+
+        [TestMethod]
+        public void TestCheckDivBy5_GoodScenario_01()
+        {
+            Assert.IsTrue(checkIfDivBy5(5));
+        }
+        [TestMethod]
+        public void TestCheckDivBy5_GoodScenario_02()
+        {
+            Assert.IsTrue(checkIfDivBy5(15));
+        }
+        [TestMethod]
+        public void TestCheckDivBy5_BadScenario_01()
+        {
+            Assert.IsFalse(checkIfDivBy5(2));
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_Fizz()
+        {
+            Assert.IsFalse(checkIfFizzBuzz(3));
+        }
+        [TestMethod]
+        public void TestFizzBuzz_Buzz()
+        {
+            Assert.IsFalse(checkIfFizzBuzz(5));
+        }
+        [TestMethod]
+        public void TestFizzBuzz_FizzBuzz()
+        {
+            Assert.IsFalse(checkIfFizzBuzz(15));
+        }
+        [TestMethod]
+        public void TestFizzBuzz_NotFizzNotBuzz()
+        {
+            Assert.IsFalse(checkIfFizzBuzz(2));
+        }
+
     }
 }
