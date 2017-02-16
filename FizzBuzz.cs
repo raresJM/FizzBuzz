7becf083bf305a877db05bcd63d251738fd65e62
raresJM <rares.scarlat.jm@gmail.com>
2017-02-17 01:33:47 +0200

    created the project FizzBuzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz2
+{
+    [TestClass]
+    public class FizzBuzz
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
